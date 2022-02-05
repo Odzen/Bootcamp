6c0b0161344e5fc55543eb482c79d4c6fcdaddeb
odzen <jsebastian.va@gmail.com>
2022-02-05 01:03:27 -0500

    Logical not

--- Javascript/2.Conditionals/2.3.Logical_Operators/app.js
+++ Javascript/2.Conditionals/2.3.Logical_Operators/app.js
@@ -14,11 +14,24 @@ if (password.length >= 6 && password.indexOf(' ') === -1) {
 // 10-65 - $20
 // 65+ - free
 
-const age = 10;
+let age = 10;
 if ((age >= 0 && age < 5) || age >= 65) {
   console.log('free');
 } else if (age >= 5 && age < 10) {
   console.log('$10');
 } else if (age >= 10 && age < 65) {
   console.log('$20');
+}
+
+//Logical not
+age = 45;
+// Another example, using the previous one
+if (!((age >= 0 && age < 5) || age >= 65)) {
+  console.log("You're not a baby or senior");
+}
+
+// If I leave empty the field, it will prompt try again
+let firstName = prompt('Enter your first name');
+if (!firstName) {
+  firstName = prompt('Try again!');
 }
