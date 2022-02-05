f5dc23e7c215affdfa9ee74f0a4cf74575045f18
odzen <jsebastian.va@gmail.com>
2022-02-05 13:54:00 -0500

    Arrays methods: push,pop

---
+++ Javascript/3.Arrays/app.js
@@ -0,0 +1,18 @@
+// Array methods
+
+//Pushing to the end
+let movieLine = ['tom', 'nancy'];
+
+movieLine.push('oliver');
+
+console.log(movieLine);
+
+movieLine.push('harry', 'hermione');
+
+console.log(movieLine);
+
+// Pop, remove from the end, doesn't require an arg
+movieLine.pop();
+console.log(movieLine);
+
+//Shift, remove from the start
