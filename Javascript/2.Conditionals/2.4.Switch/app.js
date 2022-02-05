9b3f3e40ce1b728f9fd345bfd43186e9a64429ed
odzen <jsebastian.va@gmail.com>
2022-02-05 01:16:37 -0500

    switch

---
+++ Javascript/2.Conditionals/2.4.Switch/app.js
@@ -0,0 +1,21 @@
+const day = 6;
+switch (day) {
+  case 1:
+    console.log('Moday!');
+    break;
+  case 2:
+    console.log('Tuesday!');
+    break;
+  case 3:
+    console.log('wednesday!');
+    break;
+  case 4:
+    console.log('Thursday!');
+    break;
+  case 5:
+  case 6:
+    console.log('Friday!');
+    break;
+  default:
+    console.log('Is not a day');
+}
