b5bd5136676ac4ed39d621fbf84e022682c492d4
odzen <jsebastian.va@gmail.com>
2022-02-09 21:57:49 -0500

    Shift and unshift

--- Javascript/3.Arrays/app.js
+++ Javascript/3.Arrays/app.js
@@ -1,6 +1,7 @@
 // Array methods
 
 //Pushing to the end
+console.log('Push');
 let movieLine = ['tom', 'nancy'];
 
 movieLine.push('oliver');
@@ -12,7 +13,16 @@ movieLine.push('harry', 'hermione');
 console.log(movieLine);
 
 // Pop, remove from the end, doesn't require an arg
+console.log('Pop');
 movieLine.pop();
 console.log(movieLine);
 
 //Shift, remove from the start
+console.log('Shift');
+movieLine.shift();
+console.log(movieLine);
+
+//Unshift, add to the start
+console.log('Unshift');
+movieLine.unshift('Juan');
+console.log(movieLine);
