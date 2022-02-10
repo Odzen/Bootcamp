0f049343bcdbee0f0083f87ab673b92dc49733c0
odzen <jsebastian.va@gmail.com>
2022-02-09 23:46:28 -0500

    Object Literals, accessing to them

---
+++ Javascript/4.ObjectLiterals/app.js
@@ -0,0 +1,36 @@
+// Object Literal
+
+const person = {
+  firstName: 'Juan',
+  lastName: 'Sebastian',
+};
+console.log(typeof person);
+//console.log(person);
+
+// you can use all the types
+
+let comment = {
+  username: 'odzen',
+  downVotes: 19,
+  upVotes: 214,
+  netScore: 195,
+  commentText: 'Tastes like chicken lol',
+  tags: ['#hilarius', '#funny'],
+  isGilded: false,
+};
+//console.log(comment);
+
+//Access
+//Option 1 with quotes
+console.log(comment['username']);
+//Option 2 with dot
+console.log(comment.username);
+
+//Difference between accessing data with quotes and with dot
+
+const years = { 1999: 'GOOD', 2020: 'BAD' };
+
+let birthyear = 2020;
+
+console.log(years.birthyear); // error, it does not works
+console.log(years[birthyear]); //BAD, it works
