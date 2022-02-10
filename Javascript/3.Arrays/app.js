c920fbfae2ed9b9f569f9dee1da3933b7b8736bc
odzen <jsebastian.va@gmail.com>
2022-02-09 22:24:23 -0500

    concat, includes, indexOf, reverse

--- Javascript/3.Arrays/app.js
+++ Javascript/3.Arrays/app.js
@@ -26,3 +26,26 @@ console.log(movieLine);
 console.log('Unshift');
 movieLine.unshift('Juan');
 console.log(movieLine);
+
+//concat
+console.log('Concat');
+let array1 = [1, 2, 3, 4];
+let array2 = [1, 2, 3, 5];
+let array3 = array1.concat(array2);
+console.log(array3);
+
+//includes  : true or false
+console.log('includes');
+let arrayInclude = [1, 2, 3, 4];
+console.log(arrayInclude.includes(1));
+
+//indexOf
+console.log('indexOf');
+let arrayIndex = [1, 2, 3, 4];
+console.log(arrayIndex.indexOf(1));
+console.log(arrayIndex.indexOf(5));
+
+//reverse: in place
+console.log('reverse');
+let arrayReverse = [1, 2, 3, 4];
+console.log(arrayReverse.reverse());
