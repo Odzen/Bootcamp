6772a0cd2e58d69b5bb4edc7a9d51f5576d22ebf
odzen <jsebastian.va@gmail.com>
2022-02-10 00:14:50 -0500

    Excercise

--- Javascript/4.ObjectLiterals/app.js
+++ Javascript/4.ObjectLiterals/app.js
@@ -34,3 +34,20 @@ let birthyear = 2020;
 
 console.log(years.birthyear); // error, it does not works
 console.log(years[birthyear]); //BAD, it works
+
+// Exercise
+//PLEASE DON'T TOUCH THIS LINE!
+const restaurant = {
+  name: 'Ichiran Ramen',
+  address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+  city: 'Brooklyn',
+  state: 'NY',
+  zipcode: '11206',
+};
+
+//YOUR CODE GOES DOWN HERE:
+let fullAddress = restaurant.address + ", "
+                 + restaurant.city + ", "
+                 + restaurant.state + ", "
+                 + restaurant.zipcode;
+console.log(fullAddress);
