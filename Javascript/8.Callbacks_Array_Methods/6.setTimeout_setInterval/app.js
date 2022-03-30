3cd96910b909adfe2058994adca38c35183dfbfe
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2022-03-30 00:11:14 -0500

    set timeout and set interval

---
+++ Javascript/8.Callbacks_Array_Methods/6.setTimeout_setInterval/app.js
@@ -0,0 +1,25 @@
+//In python would be something like
+// import time
+// print("Printed immediately.")
+// time.sleep(2.4)
+// print("Printed after 2.4 seconds.")
+
+//JS
+
+//SetTimeOut
+//Print Hello imediantly and
+//After 3 seconds it will ..are you still there
+console.log("Hello");
+setTimeout(() => {
+    console.log("... are you still there?");
+}, 3000);
+
+//SetInterval
+//It will call the arrow function every 2 seconds over and over again
+
+const id = setInterval(() => {
+    console.log(Math.random());
+}, 2000);
+
+
+
