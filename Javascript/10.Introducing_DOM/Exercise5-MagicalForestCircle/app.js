bface17d78dba79a699f6e79a1581ace3f0a6f83
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2022-04-12 13:14:22 -0500

    Exercise 5 - Magical forest

---
+++ Javascript/10.Introducing_DOM/Exercise5-MagicalForestCircle/app.js
@@ -0,0 +1,12 @@
+// Select the div with the id of container. Set its text aligment to center
+// Select the iamge and use JS to give it a width of 150px and a border radius of 50%
+
+
+const containerID = document.querySelector('#container')
+//console.log(containerID)
+containerID.style.textAlign = 'center'
+
+const image = document.querySelector('img')
+image.style.width = '150px'
+image.style.borderRadius = '50%'
+
