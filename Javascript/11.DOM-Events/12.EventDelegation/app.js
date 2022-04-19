1a981d98af30e4d816e72aa5f15e239c14dd5626
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2022-04-19 16:45:00 -0500

    Event delegation

---
+++ Javascript/11.DOM-Events/12.EventDelegation/app.js
@@ -0,0 +1,25 @@
+const tweetForm = document.querySelector('#tweetForm');
+const tweetsContainer = document.querySelector('#tweets');
+tweetForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const usernameInput = tweetForm.elements.username;
+    const tweetInput = tweetForm.elements.tweet;
+    addTweet(usernameInput.value, tweetInput.value)
+    usernameInput.value = '';
+    tweetInput.value = '';
+});
+
+const addTweet = (username, tweet) => {
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username)
+    newTweet.append(bTag);
+    newTweet.append(`- ${tweet}`)
+    tweetsContainer.append(newTweet);
+}
+
+// Event listener to remove the LI's inside the UL
+// In order to make it work for old and new Li's
+tweetsContainer.addEventListener('click', function (e) {
+    e.target.nodeName === 'LI' && e.target.remove();
+})
