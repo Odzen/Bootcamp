90e2a7aaaf57f887643bc2db0cc6224b2d645cf4
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2022-04-18 20:56:44 -0500

    Event listener

--- Javascript/11.DOM-Events/Lesson/app.js
+++ Javascript/11.DOM-Events/Lesson/app.js
@@ -16,3 +16,23 @@ btn.onmouseenter = scream;
 document.querySelector('h1').onclick = () => {
     alert('you clicked the h1');
 }
+
+
+// BEST OPTION -- ADD EVENT LISTENER
+// Specify the event type and callback to run
+
+const btn3 = document.querySelector('#v3');
+
+// addEventListener recives a string that indicates the input and a function
+// One click
+btn3.addEventListener('click', () => {
+    alert("Clicked the third button");
+});
+//double click
+btn3.addEventListener('dblclick', () => {
+    alert("Clicked the third button, twice");
+});
+//mouseup, when I finished to pressing the click on the mouse
+btn3.addEventListener('mouseup', () => {
+    alert("Clicked mouseup method");
+});
