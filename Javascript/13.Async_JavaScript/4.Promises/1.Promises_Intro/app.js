018fd5db6ccfe84d04dc10f7ca7a1d2f5a1c508b
Juan Sebastian Velasquez <jsebastian.va@gmail.com>
2022-04-21 00:05:42 -0500

    Creating Promises and refactoring delayed Color cahnge with promises

--- Javascript/13.Async_JavaScript/4.Promises/1.Promises_Intro/app.js
+++ Javascript/13.Async_JavaScript/4.Promises/1.Promises_Intro/app.js
@@ -104,4 +104,47 @@ fakeRequestPromise('yelp.com/api/coffee/page1')
     })
 
 
+// const delayedColorChange = (newColor, delay, doNext) => {
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = newColor;
+//         doNext && doNext();
+//     }, delay)
+// }
+
+// delayedColorChange('red', 1000, () => {
+//     delayedColorChange('orange', 1000, () => {
+//         delayedColorChange('yellow', 1000, () => {
+//             delayedColorChange('green', 1000, () => {
+//                 delayedColorChange('blue', 1000, () => {
+//                     delayedColorChange('indigo', 1000, () => {
+//                         delayedColorChange('violet', 1000, () => {
+
+//                         })
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// });
+
+// REFACTORING TO THIS, with promises
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+
+delayedColorChange('red', 1000)
+    .then(() => delayedColorChange('orange', 1000))
+    .then(() => delayedColorChange('yellow', 1000))
+    .then(() => delayedColorChange('green', 1000))
+    .then(() => delayedColorChange('blue', 1000))
+    .then(() => delayedColorChange('indigo', 1000))
+    .then(() => delayedColorChange('violet', 1000))
+
+
 
